b5459b53dc18f874278b7f3c18adcaeca2eb740f
remgee <dairymilkbatman@protonmail.com>
2026-01-25 12:59:22 +0000

    implement REPL

--- lib/main.ex
+++ lib/main.ex
@@ -3,6 +3,11 @@ defmodule CLI do
     IO.write("$ ")
     command = IO.gets("") |> String.trim()
     IO.puts("#{command}: command not found")
+    repl()
+  end
+
+  defp repl() do 
+    CLI.main("")
   end
 end
 
