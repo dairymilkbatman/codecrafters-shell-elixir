7e7768f9737e8428be95d1972a1d4875e0a35796
remgee <dairymilkbatman@protonmail.com>
2026-01-25 19:25:13 +0000

    Implement exit builtin command

--- lib/main.ex
+++ lib/main.ex
@@ -1,13 +1,23 @@
 defmodule CLI do
   def main(_args) do
+    loop()
+  end
+
+  defp loop(_, :exit) do
+    System.halt(0)
+  end
+
+  defp loop() do
     IO.write("$ ")
     command = IO.gets("") |> String.trim()
-    IO.puts("#{command}: command not found")
-    repl()
-  end
 
-  defp repl() do 
-    CLI.main("")
+    case command do
+      "exit" ->
+        loop(:ok, :exit)
+
+      _ ->
+        IO.puts("#{command}: command not found")
+        loop()
+    end
   end
 end
-
